10682040c8e67e6a4727374ad3fd9377ac26bc29
Saharak <Saharakmanoo@outlook.com>
2020-03-15 18:29:21 +0700

    [Fixed] code

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -10,23 +10,25 @@ class WebhooksController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          response = Dialogflow.send(event.message['text'])
+          resp = Dialogflow.send(event.message['text'])
           covid_total = Covid.total
 
-          if response[:intent_name] == "Covid"
-            LineBot.reply(event['replyToken'], "ทั่วโลก ติดเชื้อทั้งหมด #{covid_total[:confirmed]} คน") if response[:parameters][:confirmed].present?
-            LineBot.reply(event['replyToken'], "ทั่วโลก ติดเชื้อทั้งหมด #{covid_total[:healings]} คน") if response[:parameters][:healings].present?
-            LineBot.reply(event['replyToken'], "ทั่วโลก ติดเชื้อทั้งหมด #{covid_total[:recovered]} คน") if response[:parameters][:recovered].present?
-            LineBot.reply(event['replyToken'], "ทั่วโลก ติดเชื้อทั้งหมด #{covid_total[:deaths]} คน") if response[:parameters][:deaths].present?
-            if response[:parameters][:language].present?
+          if resp[:intent_name] == "Covid"
+            LineBot.reply(event['replyToken'], "ทั่วโลก ติดเชื้อทั้งหมด #{number_to_delimited(covid_total[:confirmed])} คน") if resp[:parameters][:confirmed].present?
+            LineBot.reply(event['replyToken'], "ทั่วโลก กำลังรักษาทั้งหมด #{number_to_delimited(covid_total[:healings])} คน") if resp[:parameters][:healings].present?
+            LineBot.reply(event['replyToken'], "ทั่วโลก รักษาหายแล้วทั้งหมด #{number_to_delimited(covid_total[:recovered])} คน") if resp[:parameters][:recovered].present?
+            LineBot.reply(event['replyToken'], "ทั่วโลก เสียชีวิตแล้วทั้งหมด #{number_to_delimited(covid_total[:deaths])} คน") if resp[:parameters][:deaths].present?
+
+            if resp[:parameters][:language].present?
               data = Covid.country('TH')
-              LineBot.reply(event['replyToken'], "คนไทย \nติดเชื้อทั้งหมด #{data[:confirmed]} คน \nกำลังรักษา #{data[:healings]} คน \nรักษาหายแล้ว #{data[:recovered]} คน \nตาย #{data[:deaths]} คน")
-              LineBot.reply(event['replyToken'], "ข้อมูลนี้ #{data[:last_updated]}")
+              LineBot.reply(event['replyToken'], "คนไทย \nติดเชื้อทั้งหมด #{number_to_delimited(data[:confirmed])} คน \nกำลังรักษา #{number_to_delimited(data[:healings])} คน \nรักษาหายแล้ว #{number_to_delimited(data[:recovered])} คน \nตาย #{number_to_delimited(data[:deaths])} คน")
             else
-              LineBot.reply(event['replyToken'], response[:fulfillment][:speech])
-            end 
+              LineBot.reply(event['replyToken'], resp[:fulfillment][:speech])
+            end
+
+            LineBot.reply(event['replyToken'], "ข้อมูลนี้ #{data[:last_updated]}")
           else
-            LineBot.reply(event['replyToken'], response[:fulfillment][:speech])
+            LineBot.reply(event['replyToken'], resp[:fulfillment][:speech])
           end
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
           response = client.get_message_content(event.message['id'])
