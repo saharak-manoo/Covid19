b6e879040113b579e08b09af1e306d6c5e4ac1ea
Saharak <Saharakmanoo@outlook.com>
2020-03-16 14:25:27 +0700

    [Fixed] code

--- config/initializers/line_bot.rb
+++ config/initializers/line_bot.rb
@@ -131,7 +131,7 @@ class LineBot
               text: header,
               size: "lg",
               weight: "bold",
-              color: "#565656"
+              color: "#565656",
               wrap: true
             }
           ]
