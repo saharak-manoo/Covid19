401b9969a1847bafe3a5794e34862368fa7023bd
Saharak <Saharakmanoo@outlook.com>
2020-03-24 13:42:22 +0700

    [Updated] code

--- app/models/thailand_summary.rb
+++ app/models/thailand_summary.rb
@@ -15,7 +15,7 @@ class ThailandSummary < ApplicationRecord
   def as_json(options = {})
     if options[:api]
       json = super().except('id')
-      json['last_updated'] = last_updated
+      json[:last_updated] = last_updated
 
       json
     else
