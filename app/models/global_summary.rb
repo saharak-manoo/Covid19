d0690a848eafb59001678440391463f845324919
Saharak <Saharakmanoo@outlook.com>
2020-03-26 15:54:32 +0700

    [Fixed] noti

--- app/models/global_summary.rb
+++ app/models/global_summary.rb
@@ -4,7 +4,7 @@ class GlobalSummary < ApplicationRecord
   def send_notification
     meesage = "\n\nจำนวนผู้ติดเชื้อ Covid19 \n- ทั่วโลก \n- เพิ่มขึ้น #{confirmed_add_today&.to_delimited || 0} คน \n\n- ติดเชื้อ #{confirmed&.to_delimited || 0} คน \n- กำลังรักษา #{healings&.to_delimited || 0} คน \n- เพิ่มขึ้น #{healings_add_today&.to_delimited || 0} คน \n- อาการหนัก #{critical&.to_delimited || 0} คน \n- เพิ่มขึ้น #{critical_add_today&.to_delimited || 0} คน \n- หายแล้ว #{recovered&.to_delimited || 0} คน \n- เพิ่มขึ้น #{recovered_add_today&.to_delimited || 0} คน \n- เสียชีวิต #{deaths&.to_delimited || 0} คน \n- เพิ่มขึ้น #{deaths_add_today&.to_delimited || 0} คน \n\n* #{DateTime.now.last_updated}"
 
-    LineNoti.send(meesage)
+    LineNoti.send(meesage) if self.changed?
   end
 
   def yesterday
