d74d51920e1cc09ab0e81d3d6ecc9f9cd37a6eeb
Saharak <saharakmanoo@outlook.com>
2020-03-28 10:34:02 +0700

    Update thailand_summary.rb

--- app/models/thailand_summary.rb
+++ app/models/thailand_summary.rb
@@ -1,5 +1,6 @@
 class ThailandSummary < ApplicationRecord
   before_save :send_notification
+  validates_uniqueness_of :date
 
   def send_notification
     meesage = "\n\nจำนวนผู้ติดเชื้อ Covid19 \n- ประเทศไทย \n- เพิ่มขึ้น #{confirmed_add_today&.to_delimited || 0} คน \n\n- ติดเชื้อ #{confirmed&.to_delimited || 0} คน \n- กำลังรักษา #{healings&.to_delimited || 0} คน \n- เพิ่มขึ้น #{healings_add_today&.to_delimited || 0} \n- อาการหนัก #{critical&.to_delimited || 0} คน \n- เพิ่มขึ้น #{critical_add_today&.to_delimited || 0} คน \n- หายแล้ว #{recovered&.to_delimited || 0} คน \n- เพิ่มขึ้น #{recovered_add_today&.to_delimited || 0} คน \n- เสียชีวิต #{deaths&.to_delimited || 0} คน \n- เพิ่มขึ้น #{deaths_add_today&.to_delimited || 0} คน \n- เฝ้าระวัง #{watch_out_collectors&.to_delimited || 0} คน \n- เพิ่มขึ้น #{watch_out_collectors_add_today&.to_delimited || 0} คน \n\n* #{DateTime.now.last_updated}"
