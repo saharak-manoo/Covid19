734542033fd9de16f1ede635f557d7d2d4ef42ba
Saharak <saharakmanoo@outlook.com>
2020-03-28 21:47:54 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => 'saharak/db/admin', as: 'rails_admin'
   devise_for :users
 
   # Angular
