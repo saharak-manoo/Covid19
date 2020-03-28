9abf4fd74ad32383dc3356a46bb98b91a5be7b21
Saharak <saharakmanoo@outlook.com>
2020-03-28 10:21:28 +0700

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,18 +1,18 @@
 class Ability
-  include CanCan::Ability
+  # include CanCan::Ability
 
-  def initialize(user)
-    can :access, :rails_admin
-    can :manage, :dashboard
-    can :read, :all
+  # def initialize(user)
+    # can :access, :rails_admin
+    # can :manage, :dashboard
+    # can :read, :all
 
-    if user.present?
-      can :manage, User, id: user.id
-      can :read, :dashboard
+    # if user.present?
+      # can :manage, User, id: user.id
+      # can :read, :dashboard
 
-      if user.has_role? :admin
-        can :manage, :all
-      end
-    end
-  end
+      # if user.has_role? :admin
+        # can :manage, :all
+      # end
+    # end
+  # end
 end
